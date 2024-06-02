7f4b6772d83e517b95b1a62c55ceb6e68199b2d6
Prassad Raj R <prassadpr10@gmail.com>
2024-06-02 23:34:45 +0530

    chg

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Transition from "../Transition/Transition";
+import Slider from "../Slider/Slider";
 function Home() {
-  return <div className="flex justify-center items-center h-[50vh]">Home</div>;
+  return (
+    <div className="flex justify-center items-center h-[50vh]">
+      <Slider />
+    </div>
+  );
 }
 
 export default Transition(Home);
